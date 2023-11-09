e6793951d3f17951df1950c165a7bba3c8de0404
Puria Nafisi Azizi <puria@dyne.org>
2023-11-09 21:51:31 +0100

    🐦‍🔥  Let's revamp this repo

--- src/utils/config.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config()
-
-export const PORT = parseInt(process.env.PORT || '3000', 10)
-export const HOST = process.env.HOST || '0.0.0.0'
-export const SMART_CONTRACTS = process.env.SMART_CONTRACTS
